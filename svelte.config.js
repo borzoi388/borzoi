0b7a9a79849a0df40760dd12016f61e05dac70c8
Hanya You <youh@catlin.edu>
2026-01-13 22:53:51 -0800

    o

--- svelte.config.js
+++ svelte.config.js
@@ -12,11 +12,10 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(
-			{pages: 'build',
-			assets: 'build',
-			fallback: '404.html',
-			precompress: false,
-			strict: true}
+			{
+				out: '',
+				fallback: '404.html',
+			}
 		),
 		appDir: "app"
 	}
